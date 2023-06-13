afeaa6a7f4793ea66ba7c48f421e6d074fc6623f
Chris Glaeser-Yalen <cyalen@gmail.com>
2023-06-13 21:55:17 +0100

    Added Flesch-Kincaid RE and GL. Began work on syllable estimator.

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use regex::Regex;
+
+pub mod readability {
+    pub fn flesch_kincaid_grade_level(words: f32, sentences: f32, syllables: f32) -> f32 {
+        (0.39 * (words / sentences)) + (11.8 * (syllables / words) - 15.59)
+    }
+
+    pub fn flesch_kincaid_reading_ease(words: f32, sentences: f32, syllables: f32) -> f32 {
+        (206.835 - (1.015 * (words / sentences))) - (84.6 * (syllables / words))
+    }
+
+    pub fn estimate_syllables(word: &str) -> () {
+        let re  = regex::Regex::new(r"[^aeiouy]+").unwrap();
+        for part in re.captures_iter(word) {
+            println!("{:?}", part)
+        }
+    }
+}
+fn main() {
+    readability::estimate_syllables("Essential");
+}
